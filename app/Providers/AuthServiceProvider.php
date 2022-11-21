a0473c55c52b8bf475c8023dda174dae069a808d
Benjamin Bick <bbick@sdv.fr>
2022-11-21 23:56:27 +0100

    trad verif mail

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 #use Illuminate\Support\Facades\Gate;
 
@@ -25,5 +28,12 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage())
+                ->subject(Lang::get('Vérification de votre adresse mail'))
+                ->line(Lang::get('Merci de cliquer sur le lien ci-dessous pour vérifier votre adresse mail.'))
+                ->action(Lang::get('Vérifier mon adresse mail'), $url)
+                ->line(Lang::get('Si vous n\'avez pas créé de compte, aucune action n\'est requise de votre part.'));
+        });
     }
 }
