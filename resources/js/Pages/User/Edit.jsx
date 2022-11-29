480ff1cc01e97bdd492350e676197064cfea1368
Benjamin Bick <bbick@sdv.fr>
2022-11-30 00:30:46 +0100

    Fix select classes dans user edit

--- resources/js/Pages/User/Edit.jsx
+++ resources/js/Pages/User/Edit.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
@@ -8,13 +8,17 @@ import {Head, Link, useForm} from '@inertiajs/inertia-react';
 import {Inertia} from "@inertiajs/inertia";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import {grades} from "@/Components/Helpers";
-
+import {Listbox, Transition} from "@headlessui/react";
+import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/20/solid";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 export default function Edit(props) {
   const {auth} = props;
   const {user} = auth;
   const initGrade = grades.find(grade => grade === auth.user.class);
   // console.log(grades.find(grade => grade === user.class));
-  const {data, setData, post,put, processing, errors, reset} = useForm({
+  const {data, setData, post, put, processing, errors, reset} = useForm({
     class: initGrade,
     password_confirmation: '',
     firstname: user.firstname || '',
@@ -41,11 +45,9 @@ export default function Edit(props) {
   };
 
 
-
-
   const submit = (e) => {
     e.preventDefault();
-    Inertia.put(route('user.update'),data);
+    Inertia.put(route('user.update'), data);
   };
 
   return (
@@ -107,8 +109,77 @@ export default function Edit(props) {
               {/*{grades.map(grade=> <span>{grade.name}</span>*/}
               {/*)}*/}
               <div className="mt-4">
-                <Select items={grades} startItem={data.class} value={data.class} name="class"
-              /></div>
+                <Listbox value={data.class} onChange={e => setData("class", e)} name="class">
+                  {({open}) => (
+                    /* TODO style du bloc */
+                    <div className="">
+                      <Listbox.Label className="block text-sm font-medium text-gray-700">Classe : </Listbox.Label>
+                      <div className="relative mt-1">
+                        <Listbox.Button
+                          className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+              <span className="flex items-center">
+                {/*<img src={selected.avatar} alt="" className="h-6 w-6 flex-shrink-0 rounded-full"/>*/}
+                <span className="ml-3 h-6 block truncate">{data.class}</span>
+              </span>
+                          <span
+                            className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+              </span>
+                        </Listbox.Button>
+
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
+                        >
+                          <Listbox.Options
+                            className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {grades.map((item) => (
+                              <Listbox.Option
+                                key={item}
+                                className={({active}) =>
+                                  classNames(
+                                    active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                                  )
+                                }
+
+                                value={item}
+                              >
+                                {({selected, active}) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      {/*<img src={person.avatar} alt=""*/}
+                                      {/*     className="h-6 w-6 flex-shrink-0 rounded-full"/>*/}
+                                      <span
+                                        className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}>
+                                            {item}
+                                              </span>
+                                    </div>
+
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active ? 'text-white' : 'text-indigo-600',
+                                          'absolute inset-y-0 right-0 flex items-center pr-4'
+                                        )}
+                                      >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                          </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                    </div>
+                  )}
+                </Listbox>
+              </div>
 
               {/*<input type="hidden" name="grade" value="1"  />*/}
               <div className="mt-4">
